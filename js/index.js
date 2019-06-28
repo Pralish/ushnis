b7de81e7005c2e833024ecfcc4e2b5d364042bc6
pralish <pralishkayastha@gmail.com>
2019-06-28 22:45:21 +0545

    parallex

--- js/index.js
+++ js/index.js
@@ -1,12 +1,32 @@
 let main = document.getElementById("main");
 let show = document.querySelector(".show");
 let bgImage = document.querySelector(".bg-image");
+let target = document.querySelectorAll(".scroll");
+
 window.addEventListener("mousemove", giveCordinates);
 function giveCordinates(e) {
-  console.log(e);
   let x = (e.pageX * -1) / 15;
   let y = (e.pageY * -1) / 20;
 
-  bgImage.style.transform = "translate3d(" + x + "px," + y + "px, 0px)";
-  // bgImage.style.transition = "transform 0.1s ease ";
+  if (window.scrollY === 0) {
+    bgImage.style.transform = "translate3d(" + x + "px," + y + "px, 0px)";
+  }
+  // else {
+  //   bgImage.style.transform = "translate3d(" + posX + "px," + posX + "px, 0px)";
+  // }
 }
+
+window.addEventListener("scroll", function(event) {
+  var posX = window.pageYOffset * 1;
+  var posY = window.pageYOffset * 2;
+
+  let index = 0,
+    length = target.length;
+
+  for (index; index < length; index++) {
+    var pos = window.pageYOffset * target[index].dataset.rate;
+
+    target[index].style.transform = "translate3d(0px," + pos + "px, 0px)";
+    bgImage.style.transform = "translate3d(" + posX + "px," + posX + "px, 0px)";
+  }
+});
